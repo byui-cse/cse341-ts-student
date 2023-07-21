5eecc5acc73ac91010a7a4d2bbeb6d123ce71637
dosmaster4life <jefferysuperman@gmail.com>
2023-07-20 19:06:29 -0600

    updated port to 8080 and added comments

--- server.ts
+++ server.ts
@@ -1,11 +1,20 @@
+// 'express' is a web application framework for Node.js, and 'Application' is a type provided by Express.
+// 'routes' is a Router instance from the 'routes' module.
 import express, { Application } from 'express';
 import routes from './routes';
 
+// Creating a new Express application instance.
 const app: Application = express();
-const port: number = Number(process.env.PORT) || 3000;
 
+// The port number on which the server will listen for requests is either defined in the environment variable 'PORT' or defaulted to 8080.
+const port: number = Number(process.env.PORT) || 8080;
+
+// Using the 'use' method to add middleware to the application's middleware stack.
+// Here, the 'routes' Router instance is added as middleware for the path '/'.
+// This means that any HTTP request will be passed to the 'routes' Router instance for handling.
 app.use('/', routes);
 
+// Starting the server to listen on the specified port.
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
