53800a03c1938c7d5043f739eae738cf1e6a3050
dosmaster4life <jefferysuperman@gmail.com>
2023-07-25 13:37:30 -0500

    added swagger and updated tsconfig and  app.ts

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import contacts from './contacts';
+import s from './swagger';
 
 // Creating a new Router instance from Express.
 const router = express.Router();
 
 // Request that starts with '/contacts' will be passed to the 'contacts' Router instance for handling.
 router.use('/contacts', contacts);
+
+// Request that starts with '/api-docs' will be passed to the 'swagger' Router instance for handling.
+router.use('/', s);
 
 export default router;
