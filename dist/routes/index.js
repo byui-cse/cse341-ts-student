d95ff7f146e717af9b8e841720707cb11e101e71
dosmaster4life <jefferysuperman@gmail.com>
2023-08-02 19:35:55 -0500

    almost complete

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const users_1 = require("../controllers/users");
-const routes = (0, express_1.Router)();
-routes.get('/', users_1.getUser);
-routes.get('/username', users_1.getUsername);
-exports.default = routes;
