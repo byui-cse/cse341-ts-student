e73a69628578790ef9fd47bbc7bab286e32fbc10
dosmaster4life <jefferysuperman@gmail.com>
2023-09-01 13:50:54 -0500

    removed unused code

--- backend/controllers/professional.ts
+++ backend/controllers/professional.ts
@@ -1,31 +1,7 @@
 //require typescript mongodb request, response, next
 import { Request, Response } from 'express';
 import { getDb } from '../db/connect';
-type Data = {
-    professionalName: string;
-    nameLink: {
-      firstName: string;
-      url: string;
-    };
-    base64Image: string;
-    firstName: string;
-    primaryDescription: string;
-    workDescription1: string;
-    workDescription2: string;
-    linkTitleText: string;
-    linkedInLink: {
-      link: string;
-      text: string;
-    };
-    githubLink: {
-      link: string;
-      text: string;
-    };
-    contactText: string;
-  };
-  
 
-  
   export const getData = (req: Request, res: Response): void => {
     const result = getDb()?.collection('user').find();
     // Transforming result to an array and sending as JSON response
@@ -34,8 +10,6 @@ type Data = {
       res.status(200).json(lists[0]);
     });
     // get data from database with mongodb
-
-    
   };
 
 
