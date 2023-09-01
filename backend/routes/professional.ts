092b6399a8cc29c8de9a3f18c55cd1e6cf8633f6
dosmaster4life <jefferysuperman@gmail.com>
2023-09-01 15:09:21 -0500

    removed unneccessary imports

--- backend/routes/professional.ts
+++ backend/routes/professional.ts
@@ -1,4 +1,4 @@
-import express, { Router, Request, Response } from 'express';
+import express, { Router } from 'express';
 import { getData } from '../controllers/professional';
 
 const router: Router = express.Router();
