73fed78e795425125dca3e3a680e5a76119f9443
Jeffery Hatch <jefferysuperman@gmail.com>
2023-11-25 15:39:04 -0700

    Delete dist directory

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const routes = (0, express_1.Router)();
-routes.get('/', (req, res) => {
-    res.send('Jenny Sanderson');
-});
-exports.default = routes;
